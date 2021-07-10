ab6bb9d68b834315611a90e02a62ea91951d1ea8
AshishMadhu <ashishmadhu26@gmail.com>
2021-07-10 14:11:44 +0530

    view update

--- chatbot/main/views.py
+++ chatbot/main/views.py
@@ -18,8 +18,8 @@ from . import models
 """
 The task question was little bit hard to understand. 
 Things I did:
-- This application can be used as a chatbot for telegram
-- This application itself contains a route to chat to chatbot (like alternative for telegram)
+- This application can be used as a chatbot server for telegram
+- This application itself contains a route that looks like a chatapp (like alternative for telegram)
 - for both cases same response
 - have a route to show availble calls to jokes and there counts with usernames
 """
